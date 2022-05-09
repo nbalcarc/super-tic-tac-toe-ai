1fec90dec9e2a6e2e0a93db11746305bb3303cf1
(Adam) Joseph Nassar <45108299+josephn619@users.noreply.github.com>
2022-05-09 16:20:49 -0700

    Documented Generation
    Done

--- ML_FinalProject/SuperTicTacToe/Generation.cs
+++ ML_FinalProject/SuperTicTacToe/Generation.cs
@@ -1,27 +1,59 @@
-using System;
-using System.Runtime.CompilerServices;
+// <copyright file="Generation.cs" company="Adam nassar &amp; Nathan Balcarcel">
+// Copyright (c) Adam nassar &amp; Nathan Balcarcel. All rights reserved.
+// </copyright>
 
 namespace SuperTicTacToe
 {
+    using System;
+    using System.Runtime.CompilerServices;
 
+    /// <summary>
+    /// Class for the Generation algorithm.
+    /// </summary>
     public class Generation
     {
-        public AI[] ais; //stores all the ais in this generation
-        public AI[] ais_back, ais_back1, ais_back2; //backups of the ais (should be ordered)
-        public int generation; //the current generation number
-        public int generation_back, generation_back1, generation_back2; //backups of the generation numbers
-        private Random random = new Random(); //"Returns a random floating-point number that is greater than or equal to 0.0, and less than 1.0."
-        private int[] scores; //stores all the scores of the ais, not used in backup however
-        public int ai_num; //number of ais per generation
+        // Stores all the ais in this generation
+        public AI[] ais;
 
-        public Generation() : this(new AI[50], -1, new AI[0], new AI[0], new AI[0], 50)
+        // Backups of the ais (should be ordered)
+        public AI[] ais_back, ais_back1, ais_back2;
+
+        // The current generation number
+        public int generation;
+
+        // Backups of the generation numbers
+        public int generation_back, generation_back1, generation_back2;
+
+        // "Returns a random floating-point number that is greater than or equal to 0.0, and less than 1.0."
+        private Random random = new Random();
+
+        // Stores all the scores of the ais, not used in backup however
+        private int[] scores;
+
+        // Number of ais per generation
+        public int aiNum;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Generation"/> class.
+        /// </summary>
+        public Generation() 
+            : this(new AI[50], -1, new AI[0], new AI[0], new AI[0], 50)
         {
 
         }
 
-        public Generation(AI[] ais, int generation, AI[] ais_back, AI[] ais_back1, AI[] ais_back2, int ai_num)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Generation"/> class.
+        /// </summary>
+        /// <param name="ais">ais.</param>
+        /// <param name="generation">generation.</param>
+        /// <param name="ais_back">ais_back.</param>
+        /// <param name="ais_back1">ais_back1.</param>
+        /// <param name="ais_back2">ais_back2.</param>
+        /// <param name="aiNum">aiNum</param>
+        public Generation(AI[] ais, int generation, AI[] ais_back, AI[] ais_back1, AI[] ais_back2, int aiNum)
         {
-            this.ai_num = ai_num;
+            this.aiNum = aiNum;
             this.generation = generation;
             this.ais = ais;
 
@@ -31,183 +63,232 @@ namespace SuperTicTacToe
             this.ais_back = ais_back;
             this.ais_back1 = ais_back1;
             this.ais_back2 = ais_back2;
-            this.scores = new int[ai_num];
+            this.scores = new int[aiNum];
 
+            // If first generation, generate new AIs to fill in the array
             if (generation == -1)
-            { //if first generation, generate new AIs to fill in the array
-                for (int i = 0; i < ai_num; i++)
+            {
+                for (int i = 0; i < aiNum; i++)
                 {
                     this.ais[i] = new AI();
                 }
-                this.generation++; //make this the first generation
+
+                // Make this the first generation
+                this.generation++;
             }
         }
 
-        //evolve in a loop, checking if a stop condition has been met or not after every new generation TODO
+        /// <summary>
+        /// Evolve in a loop, checking if a stop condition has been met or not after every new generation TODO.
+        /// </summary>
+        /// <returns>generation.</returns>
         public int Evolve()
         {
-            int generation = this.generation; //used to track how many generations we've gone through
-            //while (true) { //check stop condition here
-            //    //or check stop condition here
-            //}
+            // Used to track how many generations we've gone through
+            int generation = this.generation; 
+
             return generation;
         }
 
-        //this is where evolution takes place, have all AIs play against top 5 AIs 3 times each
+        /// <summary>
+        /// This is where evolution takes place, have all AIs play against top 5 AIs 3 times each.
+        /// </summary>
         public void NextGeneration()
         {
-            //first backup the current generations and throw out the 4th
+            // First backup the current generations and throw out the 4th
             this.ais_back2 = this.ais_back1;
             this.ais_back1 = this.ais_back;
             this.ais_back = this.ais;
-            this.ais = (AI[])this.ais.Clone(); //this may cause weird bugs, look into this later if any issues pop up
+
+            // This may cause weird bugs, look into this later if any issues pop up
+            this.ais = (AI[])this.ais.Clone(); 
+
             this.generation_back2 = this.generation_back1;
             this.generation_back1 = this.generation_back;
             this.generation_back = this.generation;
             this.generation++;
-            int[] rewards = new int[ai_num];
+            int[] rewards = new int[this.aiNum];
             (int, int) rewards_game;
             AI ai, ai1;
 
-            //now we evolve the current generation TODO
-            //if (generation == 0)
-            //{ //if the current generation is the very first, make all AIs play 3 games against 5 other AIs
-            //    int[] games = new int[500]; //stores how many games each AI has played, all must be at least 5
-
-            //}
-            //else
-            //{ //make all AIs play the top 5 AIs 3 times (used to assess if the top 5 should remain the top 5)
-            for (int i = 0; i < ai_num; i++)
+            // now we evolve the current generation TODO
+            // if (generation == 0)
+            // { //if the current generation is the very first, make all AIs play 3 games against 5 other AIs
+            //     int[] games = new int[500]; //stores how many games each AI has played, all must be at least 5
+            // }
+            // else
+            // { //make all AIs play the top 5 AIs 3 times (used to assess if the top 5 should remain the top 5)
+            for (int i = 0; i < this.aiNum; i++)
             {
-                ai = this.ais[i]; //grab the ith ai
+                // Grab the ith ai
+                ai = this.ais[i];
+
+                // All top 5 ais
                 for (int j = 0; j < 5; j++)
-                { //all top 5 ais
+                {
                     ai1 = this.ais[j];
                     rewards_game = this.PlayGames(ai, ai1);
-                    rewards[i] += rewards_game.Item1; //add the reward from this game to the total of all 5 games
+                    // Add the reward from this game to the total of all 5 games
+                    rewards[i] += rewards_game.Item1;
                 }
             }
-            //}
-            Array.Sort(rewards, ais); //sort the ais array based on the scores
 
-            //kill off about half the generation, for now just kill the bottom half, and repopulate
+            // Sort the ais array based on the scores
+            Array.Sort(rewards, ais); 
+
+            // Kill off about half the generation, for now just kill the bottom half, and repopulate
             Random r = new Random();
-            for (int i = ai_num / 2; i < ai_num; i++)
-			{
-                this.ais[i] = AI.Reproduce(this.ais[r.Next(0, ai_num / 2)], this.ais[r.Next(0, ai_num / 2)]);
-			}
+
+            for (int i = this.aiNum / 2; i < this.aiNum; i++)
+            {
+                this.ais[i] = AI.Reproduce(this.ais[r.Next(0, this.aiNum / 2)], this.ais[r.Next(0, this.aiNum / 2)]);
+            }
         }
 
-
-
-        //plays 3 games, and returns a score for each AI TODO
+        /// <summary>
+        /// Plays 3 games, and returns a score for each AI TODO.
+        /// </summary>
+        /// <param name="ai">ai.</param>
+        /// <param name="ai1">ai1.</param>
+        /// <returns>Score.</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public (int, int) PlayGames(AI ai, AI ai1)
         {
             Game game;
             Random random = new Random();
-            (int[], int[], int) rets; //tiles, tiles_inverted, focus_board, turn
             int score = 0, score1 = 0;
             (int, int) result;
             bool turn, playing;
 
+            // Play 3 games
             for (int i = 0; i < 3; i++)
-            { //play 3 games
+            {
                 game = new Game();
-                turn = random.Next(2) == 1; //choose first turn randomly
+
+                // Choose first turn randomly
+                turn = random.Next(2) == 1;
                 playing = true;
                 while (playing)
                 {
                     Console.WriteLine("trapped in this loop eyyyyyyyyyyyyyyyyyyy whats up homie");
                     if (turn)
                     {
-                        result = AIMove(ai, game);
+                        result = this.AIMove(ai, game);
                         score += result.Item2;
                     }
                     else
                     {
-                        result = AIMove(ai1, game);
+                        result = this.AIMove(ai1, game);
                         score1 += result.Item2;
                     }
+
+                    // Game over
                     if (result.Item1 == 2)
-                    { //game over
+                    {
                         playing = false;
                     }
-                    if (TieDetector(game))
-					{
+
+                    // Game over
+                    if (this.TieDetector(game))
+                    {
                         playing = false;
-					}
+                    }
+
                     turn = !turn;
                 }
             }
+
             return (score, score1);
         }
 
+        /// <summary>
+        /// Detects if tie.
+        /// </summary>
+        /// <param name="game">game.</param>
+        /// <returns>boolean.</returns>
         public bool TieDetector(Game game)
 		{
             int ties = 0, p1 = 0, p2 = 0;
             for (int i = 0; i < 9; i++)
             {
-                if (game.tiles[81 + i] == -2)
-                { //local tie
+                // Local tie
+                if (game.Tiles[81 + i] == -2)
+                {
                     ties++;
                 }
-                else if (game.tiles[81 + i] == 1)
-                { //p1
+                else if (game.Tiles[81 + i] == 1)
+                {
+                    // p1
                     p1++;
                 }
-                else if (game.tiles[81 + i] == -1)
+                else if (game.Tiles[81 + i] == -1)
                 {
                     p2++;
                 }
             }
+
             if (ties + p1 + p2 == 9)
-			{
+            {
                 return true;
-			}
+            }
+
             return false;
 
         }
 
+        /// <summary>
+        /// Simulates the AI move.
+        /// </summary>
+        /// <param name="ai">ai.</param>
+        /// <param name="game">game.</param>
+        /// <returns>blah.</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public (int, int) AIMove(AI ai, Game game)
         {
             int score = 0;
             (int[], int[], int) state = game.GetGameInfo();
+
+            // First either choose a new board or pass the current one
             if (state.Item3 == -1)
-            { //first either choose a new board or pass the current one
+            {
                 game.ChooseBoardSecondPlayer(ai.ChooseBoard(state.Item2, false));
             }
             else
             {
                 game.ChooseBoardSecondPlayer(state.Item3);
             }
-            int resultPlace, localTile, board;
-            state = game.GetGameInfo(); //update the new info
+
+            int resultPlace, localTile;
+
+            // Update the new info
+            state = game.GetGameInfo(); 
             localTile = ai.NextMove(state.Item2, state.Item3);
             resultPlace = game.PlaceSecondPlayer(localTile);
+
             switch (resultPlace)
             {
-                case -3: //board tie
+                case -3: // Board tie
                     score = 3;
                     break;
-                case 0: //successful place
+                case 0: // Successful place
                     score = -1;
                     break;
-                case 1: //local win
+                case 1: // Local win
                     score = 5;
                     break;
-                case 2: //global win
+                case 2: // G lobal win
                     score = 10;
                     break;
             }
+
             return (resultPlace, score);
         }
 
-        //used to store the last 4 generations into the database TODO
+        /// <summary>
+        /// Used to store the last 4 generations into the database TODO.
+        /// </summary>
         public void Backup()
         {
-
         }
     }
 }
