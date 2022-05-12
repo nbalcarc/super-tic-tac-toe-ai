3895c7c123b22a3bfb93848e71c207d3564153c5
nbalcarc <nathan.balcarcel@wsu.edu>
2022-05-12 16:19:36 -0700

    Revised licensing info in the stylecop and headers to match with GPL 2.0

--- ML_FinalProject/SuperTicTacToe/Board.cs
+++ ML_FinalProject/SuperTicTacToe/Board.cs
@@ -1,5 +1,17 @@
-﻿// <copyright file="Board.cs" company="Adam nassar &amp; Nathan Balcarcel">
-// Copyright (c) Adam nassar &amp; Nathan Balcarcel. All rights reserved.
+﻿// <copyright file="Board.cs" company="Adam Nassar &amp; Nathan Balcarcel">
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 // </copyright>
 
 namespace SuperTicTacToe
@@ -12,9 +24,8 @@ namespace SuperTicTacToe
     using System.Linq;
     using System.Runtime.CompilerServices;
     using System.Text;
-    using System.Threading.Tasks;
-    using System.Windows.Forms;
     using System.Threading;
+    using System.Windows.Forms;
 
     /// <summary>
     /// WinForms Interface, runs the main board.
